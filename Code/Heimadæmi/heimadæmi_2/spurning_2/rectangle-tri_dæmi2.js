ed4710c63e103468c3ec7021f44375040a0f1e0c
danielagustbjornsson <dab47@hi.is>
2023-09-02 15:54:49 +0000

    updated files

--- Code/Heimadæmi/heimadæmi_2/spurning_2/rectangle-tri_dæmi2.js
+++ Code/Heimadæmi/heimadæmi_2/spurning_2/rectangle-tri_dæmi2.js
@@ -14,8 +14,9 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var vertices = new Float32Array([-0.5, -0.25, 0.5, 0.25, -0.5, 0.25,
-									 -0.5, -0.25, 0.5, -0.25, 0.5, 0.25]);
+   /*  var vertices = new Float32Array([-0.5, -0.25, 0.5, 0.25, -0.5, 0.25,
+									 -0.5, -0.25, 0.5, -0.25, 0.5, 0.25]); */
+    var vertices = new Float32Array([-0.5, -0.25, -0.5, 0.25, 0.5, 0.25, 0.5, -0.25 ]); /* loka svar */
 
     //  Configure WebGL
 
@@ -45,5 +46,5 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 6 );
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 6 );
 }
