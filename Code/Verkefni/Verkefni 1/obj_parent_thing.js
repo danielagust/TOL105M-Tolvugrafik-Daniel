7642afe6c6d3c1faed0a84df349253649cf47de8
siggi147 <dab47@hi.is>
2023-09-11 16:48:19 +0000

    Create obj_parent_thing.js

---
+++ Code/Verkefni/Verkefni 1/obj_parent_thing.js
@@ -0,0 +1,27 @@
+class thing{
+    constructor(id, points){
+        this.id = id;
+        this.points = points;
+    }
+    get id(){
+        return this.id;
+    }
+    translatev1(vector){
+        for ( var i = 0; i < this.points.length; ++i ){
+            this.points[i] = add(this.points[i], vector);
+        }
+    }
+    translatev2(vector){
+        var temp_points = [];
+        
+        for ( var i = 0; i < this.points.length; ++i ){
+            temp_points.push(add(this.points[i], vector));
+        }
+          
+    }
+
+    move_right(amount){
+        this.translatev1(vec2(amount, 0));
+    }
+
+}
