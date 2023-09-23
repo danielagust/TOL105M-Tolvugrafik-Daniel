97a22599d467d5764c0ab62acf15df50fa54e76e
danielagustbjornsson <dab47@hi.is>
2023-09-23 13:54:44 +0000

    made the hitbox dictions

--- Code/Heimadæmi/heimadæmi_3/spurning_5/box-bounce_dæmi1.js
+++ Code/Heimadæmi/heimadæmi_3/spurning_5/box-bounce_dæmi1.js
@@ -35,16 +35,47 @@ var maxY = 1.0;
 // Hálf breidd/hæð ferningsins
 var boxRad = 0.05;
 
+var pos_spade = vec2(0.0, -0.9)
+var pos_box = vec2(0.0, -0.0)
+var box = pos_box
+
+const width_spade = 0.15;
+const height_spade = 0.02;
+
+const width_box = boxRad;
+const height_box = boxRad;
+
+var hitbox = [];
 // Ferningurinn er upphaflega í miðjunni
-var box_vertices = new Float32Array([-0.05, -0.05, 0.05, -0.05, 0.05, 0.05, -0.05, 0.05]);
+// var box_vertices = new Float32Array([-0.05, -0.05, 0.05, -0.05, 0.05, 0.05, -0.05, 0.05]);
+// var box_vertices = [
+//     vec2(-0.05, -0.05),
+//     vec2(0.05, -0.05),
+//     vec2(0.05, 0.05),
+//     vec2(-0.05, 0.05)
+// ];
 
-var spadi_vertices = [
-    vec2( -0.1, -0.9 ),
-    vec2( -0.1, -0.86 ),
-    vec2(  0.1, -0.86 ),
-    vec2(  0.1, -0.9 ) 
-];
 
+var box_vertices = [];
+box_vertices.push(add(vec2(- width_box,- height_box), pos_box)); // bottom left
+box_vertices.push(add(vec2(- width_box,height_box), pos_box)); // top left
+box_vertices.push(add(vec2( width_box, height_box), pos_box)); // top right
+box_vertices.push(add(vec2( width_box,- height_box), pos_box)); // bottom right
+
+// var spadi_vertices = [
+//     vec2( -0.1, -0.9 ),
+//     vec2( -0.1, -0.86 ),
+//     vec2(  0.1, -0.86 ),
+//     vec2(  0.1, -0.9 ) 
+// ];
+
+var spadi_vertices = [];
+
+
+spadi_vertices.push(add(vec2(- width_spade,- height_spade), pos_spade)); // bottom left
+spadi_vertices.push(add(vec2(- width_spade,height_spade), pos_spade)); // top left
+spadi_vertices.push(add(vec2( width_spade, height_spade), pos_spade)); // top right
+spadi_vertices.push(add(vec2( width_spade,- height_spade), pos_spade)); // bottom right
 // startup
 
 
@@ -93,6 +124,7 @@ window.onload = function init() {
     // Load the data into the GPU
     bufferId_box = gl.createBuffer();
     bufferId_spadi = gl.createBuffer();
+    bufferId_hitbox = gl.createBuffer();
     // gl.bindBuffer( gl.ARRAY_BUFFER, bufferId_box );
     // gl.bufferData( gl.ARRAY_BUFFER, flatten(box_vertices), gl.DYNAMIC_DRAW );
 
@@ -136,29 +168,75 @@ window.onload = function init() {
                 spadi_vertices[i][0] += xmove;
             }
 
-            gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(spadi_vertices));
+            // gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(spadi_vertices));
+            render_spade();
+            // console.log("hit", collision(box_vertices, spadi_vertices));
         }
     } );
 
+    colorLoc = gl.getUniformLocation( program, "fColor" );
+    make_hitbox();
+    // move_hitbox();
+    // // move_box();
+    // move_hitbox();
+    // move_box();
+    // move_hitbox();
+  
+    
+    
+    // render_hitbox();
     render();
 }
+var colorLoc;
+
+function collision( box_col, spadi ) {
+        
+    const ref_corner = 1;
+   
+    // return (
+    //   box1.hitbox[ref_corner].x + box1.width >= box2.hitbox[ref_corner].x && // box1 right collides with box2 left
+    //   box2.hitbox[ref_corner].x + box2.width >= box1.hitbox[ref_corner].x && // box2 right collides with box1 left
+    //   box1.hitbox[0].y + box1.height >= box2.hitbox[0].y && // box1 bottom collides with box2 top
+    //   box2.hitbox[0].y + box2.height  >= box1.hitbox[0].y // box1 top collides with box2 bottom
+    // )
+
+    return (
+        box_col[ref_corner][0] + width_box >= spadi[ref_corner][0] && // box right collides with spadi left
+        spadi[ref_corner][0] + width_spade >= box_col[ref_corner][0] && // spadi right collides with box left
+        box_col[0][1] + height_box >= spadi[0][1] && // box bottom collides with spadi top
+        spadi[0][1] + height_spade  >= box_col[0][1] // box top collides with spadi bottom
+      )
+
+  }
+
 var test = vec2(0.0,1.0);
 
-function render() {
-    
-    // Láta ferninginn skoppa af veggjunum
-    if (Math.abs(box[0] + dX) > maxX - boxRad) dX = -dX;
-    if (Math.abs(box[1] + dY) > maxY - boxRad) dY = -dY;
+function render_spade(){
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId_spadi );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(spadi_vertices), gl.DYNAMIC_DRAW );
 
-    // Uppfæra staðsetningu
-    box[0] += dX;
-    box[1] += dY;
-    
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    //
-    // test[0] += 1.0 ;
+    gl.uniform2fv( if_spadi, flatten(test) );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+}
 
+var bufferId_hitbox;
+function render_hitbox(){
     // box
+    gl.uniform4fv( colorLoc, vec4(0.0, 0.0, 1.0, 1.0) );  
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId_hitbox );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(hitbox), gl.DYNAMIC_DRAW );
+    // gl.uniform1fv( if_spadi, flatten(vec2(0.0,0.0)) );    
+    // gl.uniform2fv( locBox, flatten(box) );
+    gl.uniform2fv( if_spadi, flatten( vec2(0.0,1.0)) );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+}
+
+
+function render_box(){
+    // box
+    gl.uniform4fv( colorLoc, vec4(1.0, 0.0, 0.0, 1.0) );  
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId_box );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(box_vertices), gl.DYNAMIC_DRAW );
@@ -166,14 +244,105 @@ function render() {
     gl.uniform2fv( locBox, flatten(box) );
     gl.uniform2fv( if_spadi, flatten( vec2(0.0,0.0)) );
     gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+}
+
+function move_box(){
+    // Láta ferninginn skoppa af veggjunum
+    if (Math.abs(box[0] + dX) > maxX - boxRad) dX = -dX;
+    if (Math.abs(box[1] + dY) > maxY - boxRad) dY = -dY;
+    
+
+    // Uppfæra staðsetningu
+    box[0] += dX;
+    box[1] += dY;
+    // console.log("hello");
+    
+    
+}
+
+function make_hitbox(){
+    hitbox.push(add(vec2(- width_box,- height_box), pos_box)); // bottom left
+    hitbox.push(add(vec2(- width_box,height_box), pos_box)); // top left
+    hitbox.push(add(vec2( width_box, height_box), pos_box)); // top right
+    hitbox.push(add(vec2( width_box,- height_box), pos_box)); // bottom right
+}
+function move_hitboxv2(){
+    hitbox[0] = (add(vec2(- width_box,- height_box), box)); // bottom left
+    hitbox[1] = (add(vec2(- width_box,height_box), box)); // top left
+    hitbox[2] = (add(vec2( width_box, height_box), box)); // top right
+    hitbox[3] = (add(vec2( width_box,- height_box), box)); // bottom right 
+    // for ( var i = 0; i < hitbox.length; ++i ){
+    //     hitbox[i][0] += box[0];
+    //     hitbox[i][1] += box[1];
+    // }
+    
+}
+
+function move_hitbox(){
+    move_hitboxv2();
+    // translatev1(box);
+}
+
+function translatev1(vector){
+    for ( var i = 0; i < hitbox.length; ++i ){
+        hitbox[i] = add(hitbox[i], vector);
+    }
+}
 
 
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId_spadi );
+// move_box();
+// move_hitbox();
+// move_box();
+// move_hitbox();
+
+function get_top_cornor(){
+    return vec2(box[0]-width_box, box[1]+height_box);
+}
+function get_bottom_coronor(){
+
+    return vec2(box[0]-width_box, box[1]-height_box);
+}
+
+function render() {
+    
+    
+    //
+    // test[0] += 1.0 ;
+    // move_box();
+    move_box();
+    
+
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    
+
+    //box
+    // render_hitbox();
+    
+    move_hitbox();
+    render_hitbox();
+    
+    
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(spadi_vertices), gl.DYNAMIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(get_top_cornor()), gl.DYNAMIC_DRAW );
+    // gl.uniform1fv( if_spadi, flatten(vec2(0.0,0.0)) ); 
+    gl.uniform4fv( colorLoc, vec4(0.0, 1.0, 0.0, 1.0) );   
+    // gl.uniform2fv( locBox, flatten(box) );
+    gl.uniform2fv( if_spadi, flatten( vec2(0.0,0.0)) );
+    gl.drawArrays( gl.POINTS, 0, 1 );
 
-    gl.uniform2fv( if_spadi, flatten(test) );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    render_box();
+   
+   
+
+    // spadi
+    render_spade();
+    // console.log(hitbox);
+    console.log("hit", collision(hitbox, spadi_vertices));
+
+   
     
 
 
--- Code/Verkefni/Verkefni 1/obj_parent_char.js
+++ Code/Verkefni/Verkefni 1/obj_parent_char.js
@@ -175,32 +175,9 @@ export default class char_parent{
     } 
 
     collision( box1, box2 ) {
-        // console.log("box1");
-        // console.log(
-        //     box1.top_cornor.x + box1.width >= box2.top_cornor.x && // box1 right collides with box2 left
-        //     box2.top_cornor.x + box2.width >= box1.top_cornor.x );
+        
         const ref_corner = 1;
-        // box1 right collides with box2 left
-        // console.log("box 1 this x bool ", box1.hitbox[ref_corner].x + box1.width >= box2.hitbox[ref_corner].x);
-        // console.log("box 1 this x value ", box1.hitbox[ref_corner].x + box1.width);
-        // console.log("");
-
-        // // box2 right collides with box1 left
-        // console.log("box 2 this x bool ", box2.hitbox[ref_corner].x + box2.width >= box1.hitbox[ref_corner].x);
-        // console.log("box 2 this x value ", box2.hitbox[ref_corner].x + box2.width);
-        // console.log("");
-
-        // // box1 bottom collides with box2 top
-        // console.log("box 1 this y bool ", box1.hitbox[ref_corner].y + box1.height >= box2.hitbox[ref_corner].y);
-        // console.log("box 1 this y value ", box1.hitbox[ref_corner].y + box1.height);
-        // console.log("");
-
-        // // box1 top collides with box2 bottom
-        // console.log("box 2 this y bool ",  box2.hitbox[ref_corner].y + box2.height  >= box1.hitbox[ref_corner].y);
-        // console.log("box 2 this y value ",  box2.hitbox[ref_corner].y + box2.height);
-        // console.log("");
-
-        // console.log("")
+       
 
         // return (
         //   box1.hitbox[ref_corner].x + box1.width >= box2.hitbox[ref_corner].x && // box1 right collides with box2 left
