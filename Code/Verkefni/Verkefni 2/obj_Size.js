8fcd46240b64851a3c4da57cfdbf7dd29fa8b4e0
danielagustbjornsson <dab47@hi.is>
2023-10-03 22:17:49 +0000

    Update obj_Size.js

--- Code/Verkefni/Verkefni 2/obj_Size.js
+++ Code/Verkefni/Verkefni 2/obj_Size.js
@@ -5,38 +5,43 @@ export default class obj_Size{
      * @param {number} height 
      * @param {number} width 
      */
-    constructor(length, height,width=0.0){
+    constructor(length, height,width=0.0, thicknes=0.0){
         this.length = length;
         this.height = height;
         this.width = width;
+        this.thicknes = thicknes;
     }
 
-    get position2d(){
+    get size2d(){
         return [this.length, this.height];
     }
 
-    set position2d(new_size){
+    set size2d(new_size){
         this.length = new_size[0];
         this.height = new_size[1];
     }
 
-    get position2d_to_vec(){
+    get size2d_to_vec(){
         return vec2(this.length, this.height);
     }
 
 
-    get position3d(){
+    get size3d(){
         return [this.length, this.height, this.width];
     }
 
-    set position3d(new_size){
+    set size3d(new_size){
         this.length = new_size[0];
         this.height = new_size[1];
         this.width = new_size[2];
     }
 
-    get position3d_to_vec(){
+    get size3d_to_vec(){
         return vec3(this.length, this.height, this.width);
+    }
+
+    get size_with_thickens(){
+        return [this.length, this.height, this.width, this.thicknes];
     }
     
 }
