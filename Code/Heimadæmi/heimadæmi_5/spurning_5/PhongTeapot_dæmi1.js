6dc570e8e93d72e039f98266671dbdc23fc309c3
danielagustbjornsson <dab47@hi.is>
2023-10-21 21:26:58 +0000

    Update PhongTeapot_dæmi1.js

--- Code/Heimadæmi/heimadæmi_5/spurning_5/PhongTeapot_dæmi1.js
+++ Code/Heimadæmi/heimadæmi_5/spurning_5/PhongTeapot_dæmi1.js
@@ -40,8 +40,8 @@ var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
 var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
 var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
-var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
-var materialShininess = 150.0;
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 ); //vec4( 1.0, 1.0, 1.0, 1.0 ); vec4( 0.0, 0.0, 0.0, 1.0 )
+var materialShininess = 150.0;//150
 
 var flag = true;
 
