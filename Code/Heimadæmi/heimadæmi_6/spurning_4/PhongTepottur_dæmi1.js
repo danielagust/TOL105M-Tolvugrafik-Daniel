706582f5f32457d895f4b3d9e71f486d03353329
danielagustbjornsson <dab47@hi.is>
2023-10-28 13:15:56 +0000

    fixed some stuff

--- Code/Heimadæmi/heimadæmi_6/spurning_4/PhongTepottur_dæmi1.js
+++ Code/Heimadæmi/heimadæmi_6/spurning_4/PhongTepottur_dæmi1.js
@@ -49,37 +49,68 @@ var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-window.onload = function init() {
+var num_iteration =5;
 
+var vBuffer;
+var nBuffer
+var myTeapot = teapot(num_iteration);
+onload = function init(){
     canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
+
     gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
 
-    gl.enable(gl.DEPTH_TEST);
-    //gl.enable(gl.CULL_FACE);
-    gl.cullFace(gl.BACK);
+    vBuffer = gl.createBuffer();
+    nBuffer = gl.createBuffer();
 
-
-    var myTeapot = teapot(15);
-    myTeapot.scale(0.5, 0.5, 0.5);
-
-    console.log(myTeapot.TriangleVertices.length);
-
-    points = myTeapot.TriangleVertices;
-    normals = myTeapot.Normals;
-
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+    gl.enable(gl.DEPTH_TEST);
+    //event listeners for mouse
+    event_keyboard()
+    run();
+}
 
+
+
+function get_light(){
+    // lightPosition = vec4(x_pos, y_pos, z_pos, 0.0 );
 
     ambientProduct = mult(lightAmbient, materialAmbient);
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
     specularProduct = mult(lightSpecular, materialSpecular);
 
+    gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
+
+
+}
+function run() {
+
+    
+
+    myTeapot = teapot(num_iteration);
+    myTeapot.scale(0.5, 0.5, 0.5);
+
+    console.log(myTeapot.TriangleVertices.length);
+    
+    // myTeapot = teapot(num_iteration);
+    points = myTeapot.TriangleVertices;
+    normals = myTeapot.Normals;
+
+
+    
+    // ambientProduct = mult(lightAmbient, materialAmbient);
+    // diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    // specularProduct = mult(lightSpecular, materialSpecular);
+    get_light()
 
     var nBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
@@ -105,13 +136,12 @@ window.onload = function init() {
     projectionMatrix = perspective( fovy, 1.0, near, far );
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
-    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
+    
+    
+    render();
+}
 
-    //event listeners for mouse
+function event_keyboard(){
     canvas.addEventListener("mousedown", function(e){
         movement = true;
         origX = e.clientX;
@@ -141,9 +171,36 @@ window.onload = function init() {
          }
      }  );
 
-    render();
+     window.addEventListener("keydown", function(e){
+        
+        switch( e.keyCode ) {
+           case 38:	// upp ör
+               zDist += 1.0;
+               break;
+           case 40:	// niður ör
+               zDist -= 1.0;
+               break;
+           // case 90:	// z - snýr stöpli áfram
+           //     theta[0] = Math.min(180, theta[0]+5);
+           //     break;
+           // case 88:	// x - snýr stöpli afturábak
+           //     theta[0] = Math.max(-180, theta[0]-5);
+           //     break;
+           case 65:	// a - snýr neðri armi
+               neðri_theta = Math.min(max_neðri_theta, neðri_theta+5);
+               break;
+           case 83:	// s - snýr neðri armi
+               neðri_theta = Math.max(-min_neðri_theta, neðri_theta-5);
+               break;
+           case 81:	// q - snýr efri armi
+               efri_theta = Math.min(max_efri_theta, efri_theta+5);
+               break;
+           case 87:	// w - snýr efri armi
+               efri_theta = Math.max(-min_efri_theta, efri_theta-5);
+               break;
+        }
+    }  );
 }
-
 
 function render() {
 
@@ -152,6 +209,7 @@ function render() {
     modelViewMatrix = lookAt( vec3(0.0, 0.0, zDist), at, up );
     modelViewMatrix = mult( modelViewMatrix, rotateY( -spinY ) );
     modelViewMatrix = mult( modelViewMatrix, rotateX( spinX ) );
+    
 
     normalMatrix = [
         vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
