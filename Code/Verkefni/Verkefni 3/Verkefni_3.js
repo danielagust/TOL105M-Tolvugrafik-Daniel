919321654417a0ff44c03606f758d506495f27be
danielagustbjornsson <dab47@hi.is>
2023-11-06 19:11:19 +0000

    renamed

---
+++ Code/Verkefni/Verkefni 3/Verkefni_3.js
@@ -0,0 +1,61 @@
+// import * as THREE from "../../ThreeJS/js/three.js";
+// import { OrbitControls } from "../../ThreeJS/js/examples/jsm/controls/OrbitControls.js";
+// Ná í striga
+const canvas = document.querySelector('#c');
+
+// Skilgreina sviðsnet
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('skyblue');
+
+// Skilgreina myndavél og staðsetja hana
+const camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth/canvas.clientHeight, 0.1, 1000 );
+camera.position.set(0, 1, 3);
+
+// Bæta við músarstýringu
+const controls = new THREE.OrbitControls( camera, canvas );
+
+// controls.lookSpeed = 0.1;
+// controls.movementSpeed = 10;
+
+var clock = new THREE.Clock(true);
+
+// Skilgreina birtingaraðferð með afbjögun (antialias)
+const renderer = new THREE.WebGLRenderer({canvas, antialias:true});
+
+// Búa til tening með Phong áferð (Phong material) og bæta í sviðsnetið
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } );
+const cube = new THREE.Mesh( geometry, material );
+cube.position.x += 1;
+scene.add( cube );
+
+// Búa til kúlu með Phong áferð og bæta í sviðsnetið
+const ballGeometry = new THREE.SphereGeometry( 0.5, 20, 20 );
+const ballMaterial = new THREE.MeshPhongMaterial( { color: 0xaa8844 } );
+const ball = new THREE.Mesh( ballGeometry, ballMaterial );
+ball.position.x += -1;
+scene.add( ball );
+
+// Búa til sléttu með Phong áferð
+const planeGeometry = new THREE.PlaneGeometry( 4, 2 );
+const planeMaterial = new THREE.MeshPhongMaterial( { color: 0xcccccc } );
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.rotation.x = -0.5 * Math.PI;
+plane.position.set(0, -0.5, 0);
+scene.add( plane );
+
+
+// Skilgreina ljósgjafa og bæta honum í sviðsnetið
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(2, 4, 1);
+scene.add(light);
+
+// Hreyfifall
+const animate = function () {
+    requestAnimationFrame( animate );
+
+    controls.update(clock.getDelta());
+    renderer.render( scene, camera );
+};
+
+animate();
